ce72180ee9904cf04030b00c7a434a5813433cb9
Jim Hoskins <jim@teamtreehouse.com>
2012-07-31 10:28:58 -0400

    Added custon routes for auth and feed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Treebook::Application.routes.draw do
   devise_for :users
 
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'login', to: 'devise/sessions#new', as: :login
+    get 'logout', to: 'devise/sessions#destroy', as: :logout
+  end
+
   resources :statuses
+  get 'feed', to: 'statuses#index', as: :feed
   root to: 'statuses#index'
 
   # The priority is based upon order of creation:
