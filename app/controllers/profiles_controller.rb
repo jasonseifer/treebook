4e0eee98abb0dafbd0723c96af48a5a794b5a889
Jason Seifer <me@jasonseifer.com>
2012-07-31 18:18:19 -0400

    Scope user statuses correctly

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
   def show
     @user = User.find_by_profile_name(params[:id])
     if @user
-      @statuses = Status.all
+      @statuses = @user.statuses.all
       render action: :show
     else
       render file: 'public/404', status: 404, formats: [:html]
--- test/functional/profiles_controller_test.rb
+++ test/functional/profiles_controller_test.rb
@@ -17,5 +17,12 @@ class ProfilesControllerTest < ActionController::TestCase
     assert_not_empty assigns(:statuses)
   end
 
+  test "only shows the correct user's statuses" do
+    get :show, id: users(:jason).profile_name
+    assigns(:statuses).each do |status|
+      assert_equal users(:jason), status.user
+    end
+  end
+
 
 end
