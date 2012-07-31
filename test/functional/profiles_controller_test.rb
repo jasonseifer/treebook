440478b5d44c9b4fa51452810f2a18270d0a7c64
Jason Seifer <me@jasonseifer.com>
2012-07-31 18:15:27 -0400

    Add profile page and route

--- test/functional/profiles_controller_test.rb
+++ test/functional/profiles_controller_test.rb
@@ -1,9 +1,21 @@
 require 'test_helper'
 
 class ProfilesControllerTest < ActionController::TestCase
-  test "should get show" do
-    get :show
+  test "should find a profile" do
+    get :show, id: users(:jason).profile_name
     assert_response :success
+    assert_template 'profiles/show'
   end
+
+  test "should render a 404 on no profile found" do
+    get :show, id: 'doesnt exist'
+    assert_response :not_found
+  end
+
+  test "shows statuses on success" do
+    get :show, id: users(:jason).profile_name
+    assert_not_empty assigns(:statuses)
+  end
+
 
 end
