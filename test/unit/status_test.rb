f2cb5ad2ec9586d720b2be6bd7487fe641004713
Jason Seifer <me@jasonseifer.com>
2012-07-31 12:26:37 -0400

    Add status validations and tests

--- test/unit/status_test.rb
+++ test/unit/status_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class StatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "that a status requires content" do
+    status = Status.new
+    assert !status.save
+    assert !status.errors[:content].empty?
+  end
+
+  test "that a status's content is at least 2 letters" do
+    status = Status.new
+    status.content = "H"
+    assert !status.save
+    assert !status.errors[:content].empty?
+  end 
+
+  test "that a status has a user id" do
+    status = Status.new
+    status.content = "Hello"
+
+    assert !status.save
+    assert !status.errors[:user_id].empty?
+  end
 end
