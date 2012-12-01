887c297cbdb7b47416ba2174b45dcbdda73f0f5a
Jason Seifer <me@jasonseifer.com>
2012-11-30 19:26:56 -0500

    Finish up user friendship testing

---
+++ test/integration/add_a_friend_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class AddAFriendTest < ActionDispatch::IntegrationTest
+  def sign_in_as(user, password)
+    post login_path, user: { email: user.email, password: password }
+  end
+
+  test "that adding a friend works" do
+    sign_in_as users(:jason), "testing"
+
+    get "/user_friendships/new?friend_id=#{users(:jim).profile_name}"
+    assert_response :success
+
+    assert_difference 'UserFriendship.count' do
+      post "/user_friendships", user_friendship: { friend_id: users(:jim).profile_name }
+      assert_response :redirect
+      assert_equal "You are now friends with #{users(:jim).full_name}", flash[:success]
+    end
+  end
+end
